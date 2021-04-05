7c60fe6cb217caef4cfff0395fdc4ebc35a2eee3
ArafaAbRahim <arafaarahim4@gmail.com>
2021-04-05 13:40:13 +0600

    Create layout and router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { Container, Grid } from '@material-ui/core';
 import React from 'react'
 import './App.css';
 
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Resume from './pages/Resume/Resume';
+import Footer from './components/Footer/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      our resume website 
-    </div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3}>
+          <Profile />
+        </Grid>
+        <Grid item xs>
+          <Header />
+          <Router>
+            <Switch>
+              <Route path='/portfolio'>
+                <Portfolio />
+              </Route>
+              <Route path='/'>
+                <Resume />
+            </Route>    
+            </Switch>                                
+          </Router>          
+          <Footer />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
